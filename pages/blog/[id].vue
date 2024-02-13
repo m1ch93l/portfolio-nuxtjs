a5b85fe2c9fa34bf24a5cdd7b19a74db7b891414
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-02-13 20:46:46 +0800

    Use params and links

---
+++ pages/blog/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        We are inside a blog!
+    </div>
+    <div>
+        Post id is {{ route.params.id }}
+    </div>
+</template>
+
+<script setup>
+
+const route = useRoute()
+
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This is all blogs!
+    </div>
+</template>
